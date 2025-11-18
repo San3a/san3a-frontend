99cf79be9ac227bb3eddcd4b2e99d5ad4b74dfac
yousefmobelal <ym8370302@gmail.com>
2025-11-18 12:11:26 +0200

    Enhance AddPost component, modify translations, and add PostsPage route

--- src/features/posts/components/AddPost.jsx
+++ src/features/posts/components/AddPost.jsx
@@ -13,7 +13,6 @@ function AddPost() {
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
 
-  console.log(`This is the data of user: ${JSON.stringify(user)}`);
   return (
     <>
       <div
@@ -21,10 +20,14 @@ function AddPost() {
           theme === "dark" ? "bg-[#252728]" : "bg-white"
         } p-8 rounded-xl shadow-lg w-full max-w-lg flex justify-center items-center gap-4 `}
       >
-        <img
-          className="h-12 w-12 bg-black rounded-full shrink-0"
-          src={user.image}
-        />
+        {user?.image === null ? (
+          <div className="h-12 w-12 bg-blue-600 rounded-full shrink-0"></div>
+        ) : (
+          <img
+            className="h-12 w-12 bg-black rounded-full shrink-0"
+            src="https://plus.unsplash.com/premium_photo-1755882951408-b6d668ccca21?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        )}
         <button
           onClick={handleOpenModal}
           className={`h-[80%] ${
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -12,6 +12,7 @@ import Users from "../features/admin/pages/Users";
 import Categories from "../features/admin/pages/Categories";
 import Reviews from "../features/admin/pages/Reviews";
 import Layout from "../features/admin/components/Layout";
+import PostsPage from "../features/posts/PostsPage";
 
 const AppRoutes = () => {
   return (
@@ -23,6 +24,7 @@ const AppRoutes = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/test" element={<TestPage />}></Route>
+        <Route path="/posts" element={<PostsPage />}></Route>
         <Route path="/chat/:conversationId" element={<ChatPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
